8c1c745a96bfdf392bb4e3dfb2b40f5e744a228e
alexhdz14 <116674460+alexhdz14@users.noreply.github.com>
2024-11-21 16:49:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import re
 
 app = Flask(__name__)
-app.secret_key = 'your_secure_secret_key_here'
+app.secret_key = 'secure_secret_key'
 
 # Load the spaCy model for NLP processing
 nlp = spacy.load('en_core_web_sm')
